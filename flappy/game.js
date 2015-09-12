9a3eb9ee589bb7a08798c68c2e3e171f95478ba7
Jakob Bassinger <jemoryb@gmail.com>
2015-09-12 12:01:18 -0400

    save

--- flappy/game.js
+++ flappy/game.js
@@ -3,16 +3,20 @@
 var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
     {preload:preload,create:create,update:update,render:render});
 
-var background
+var background;
+
+var flappy;
 
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
-} game.load.image("background", "background.gif");
+  game.load.image("background", "background.gif");
+  game.load.image("flappy", "flappy.gif");
+}
 
 function create() {
   background = game.add.tileSprite(0,0,320,568,'background');
   background.autoScroll(-100,0);
-}
+  flappy = game.add.Sprite(40,40,'flappy'); }
 
 function update() {
 }
